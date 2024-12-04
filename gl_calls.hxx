3f8cc368466960cf1e078bb57369fd6a3a5e5c94
ateraz13 <mipsmartin@gmail.com>
2024-12-04 08:05:43 +0000

    Debugging GL calls.
    These are generated by the gen_precalls.sh script.
    Any OpenGL call prefixed with the letter 'd' get
    a wrapper generated that calls a precall and postcall callback.
    Basically a function gets executed before and after the GL call.
    This is mostly useful for debugging purposes. The wrapper also prints
    the location where the GL function has been called including the source
    file and the line number. All the parameters that have been passed
    into the function are printed for debugging purposes as well as the
    return values of the functions that do return values.
    
    It was primarily meant to be used to visualise the executing order
    and the parameters passed into the GL functions for debugging purposes.
    I am sure however there will be many other uses where this will come in
    handy. It also allows the functionality to be selectively enabled
    only for some GL calls by simply adding or removing the letter 'd'
    prefix to any OpenGL function. Techically this could be expanded to work
    with any function but it is not supported at the moment.
    
    The script (gen_precalls.sh) is rather simple but it serves its purpose.

---
+++ gl_calls.hxx
@@ -0,0 +1,64 @@
+#ifndef GL_CALLS_HXX
+#define GL_CALLS_HXX
+#include <iostream>
+#include <type_traits>
+#define DBG_GL_PRE_CALLBACK precall_callback
+#define DBG_GL_POST_CALLBACK postcall_callback
+void precall_callback(const char* source_file, int line_num, const char* gl_func);
+void postcall_callback(const char* source_file, int line_num, const char* gl_func);
+ 
+void dbg_gl_print_args_internal() {}
+ 
+template<typename Arg, typename... Rest>
+void dbg_gl_print_args_internal(Arg arg, Rest... rest) {
+   std::cout << ", " << arg;
+   dbg_gl_print_args_internal(rest...);
+}
+ 
+void dbg_gl_print_args() {}
+ 
+template<typename Arg, typename... Rest>
+void dbg_gl_print_args(Arg arg, Rest... rest) {
+   std::cout <<  arg;
+   dbg_gl_print_args_internal(rest...);
+}
+ 
+// General GL_Call template
+template <typename GL_Func, typename... Args>
+static std::invoke_result_t<GL_Func, Args...> dbg_gl_call(GL_Func gl_func, const char* source_file, int line_num, const char* func_name, Args... args) {
+   std::cout << source_file << ":" << line_num << ": " << func_name << "(";
+   dbg_gl_print_args(args...);
+   std::cout << ")\n";
+   DBG_GL_PRE_CALLBACK(source_file, line_num, func_name);
+
+   if constexpr (std::is_same_v<std::invoke_result_t<GL_Func, Args...>, void>) {
+       gl_func(args...); // No return value
+       DBG_GL_POST_CALLBACK(source_file, line_num, func_name);
+   } else {
+       auto ret = gl_func(args...); // Capture return value
+       DBG_GL_POST_CALLBACK(source_file, line_num, func_name);
+       std::cout << "gl_call_return: " << ret << "\n";
+       return ret;
+   }
+}
+#define dglBindBuffer(args...) \
+  dbg_gl_call(glBindBuffer, __FILE__, __LINE__, "glBindBuffer", args)
+#define dglBindVertexArray(args...) \
+  dbg_gl_call(glBindVertexArray, __FILE__, __LINE__, "glBindVertexArray", args)
+#define dglBufferData(args...) \
+  dbg_gl_call(glBufferData, __FILE__, __LINE__, "glBufferData", args)
+#define dglDeleteBuffers(args...) \
+  dbg_gl_call(glDeleteBuffers, __FILE__, __LINE__, "glDeleteBuffers", args)
+#define dglDeleteVertexArrays(args...) \
+  dbg_gl_call(glDeleteVertexArrays, __FILE__, __LINE__, "glDeleteVertexArrays", args)
+#define dglDrawElements(args...) \
+  dbg_gl_call(glDrawElements, __FILE__, __LINE__, "glDrawElements", args)
+#define dglEnableVertexAttribArray(args...) \
+  dbg_gl_call(glEnableVertexAttribArray, __FILE__, __LINE__, "glEnableVertexAttribArray", args)
+#define dglGenBuffers(args...) \
+  dbg_gl_call(glGenBuffers, __FILE__, __LINE__, "glGenBuffers", args)
+#define dglGenVertexArrays(args...) \
+  dbg_gl_call(glGenVertexArrays, __FILE__, __LINE__, "glGenVertexArrays", args)
+#define dglVertexAttribPointer(args...) \
+  dbg_gl_call(glVertexAttribPointer, __FILE__, __LINE__, "glVertexAttribPointer", args)
+#endif //GL_CALLS_HXX
