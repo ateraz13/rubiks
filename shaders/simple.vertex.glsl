524f1c126622e204508216938da46b0ba9e6756b
ateraz13 <mipsmartin@gmail.com>
2024-12-04 08:04:09 +0000

    Use MVP and provide default identity matrix.

--- shaders/simple.vertex.glsl
+++ shaders/simple.vertex.glsl
@@ -1,12 +1,19 @@
 #version 450
 
+layout(location=25) uniform mat4 mvp = mat4(
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 1
+);
+
 layout(location=0) in vec3 vertex_pos;
 layout(location=1) in vec4 vertex_color;
 
-layout(location=10) out vec4 frag_color;
-layout(location=11) out vec4 frag_pos;
+layout(location=10) out vec4 frag_pos;
+layout(location=11) out vec4 frag_color;
 
 void main() {
     frag_color = vertex_color;
-    frag_pos = vec4(vertex_pos, 1.0);
+    frag_pos = mvp * vec4(vertex_pos, 1.0);
 }
