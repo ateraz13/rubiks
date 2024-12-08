d2b1644c3bf4541fbec7f0adba776a7fa2825b05
ateraz13 <mipsmartin@gmail.com>
2024-12-08 10:24:52 +0000

    Make gl_calls usable across multiple files.

---
+++ gl_calls.cxx
@@ -0,0 +1,4 @@
+#include "gl_calls.hxx"
+
+void dbg_gl_print_args_internal () {}
+void dbg_gl_print_args () {}
--- gl_calls.hxx
+++ gl_calls.hxx
@@ -1,31 +1,32 @@
 #ifndef GL_CALLS_HXX
 #define GL_CALLS_HXX
+
 #include <iostream>
 #include <type_traits>
 #define DBG_GL_PRE_CALLBACK precall_callback
 #define DBG_GL_POST_CALLBACK postcall_callback
 void precall_callback(const char* source_file, int line_num, const char* gl_func);
 void postcall_callback(const char* source_file, int line_num, const char* gl_func);
- 
-void dbg_gl_print_args_internal() {}
- 
-template<typename Arg, typename... Rest>
-void dbg_gl_print_args_internal(Arg arg, Rest... rest) {
-   std::cout << ", " << arg;
-   dbg_gl_print_args_internal(rest...);
-}
- 
-void dbg_gl_print_args() {}
- 
-template<typename Arg, typename... Rest>
-void dbg_gl_print_args(Arg arg, Rest... rest) {
+
+void dbg_gl_print_args_internal ();
+void dbg_gl_print_args ();
+
+template<typename Arg, typename... Args>
+void dbg_gl_print_args_internal (Arg arg, Args... rest) {
+  std::cout << ", " << arg;
+  dbg_gl_print_args_internal(rest...);
+};
+
+template<typename Arg, typename... Args>
+void dbg_gl_print_args (Arg arg, Args... rest) {
    std::cout <<  arg;
    dbg_gl_print_args_internal(rest...);
-}
- 
+};
+
 // General GL_Call template
 template <typename GL_Func, typename... Args>
 static std::invoke_result_t<GL_Func, Args...> dbg_gl_call(GL_Func gl_func, const char* source_file, int line_num, const char* func_name, Args... args) {
+
    std::cout << source_file << ":" << line_num << ": " << func_name << "(";
    dbg_gl_print_args(args...);
    std::cout << ")\n";
@@ -41,24 +42,18 @@ static std::invoke_result_t<GL_Func, Args...> dbg_gl_call(GL_Func gl_func, const
        return ret;
    }
 }
-#define dglBindBuffer(args...) \
-  dbg_gl_call(glBindBuffer, __FILE__, __LINE__, "glBindBuffer", args)
-#define dglBindVertexArray(args...) \
-  dbg_gl_call(glBindVertexArray, __FILE__, __LINE__, "glBindVertexArray", args)
-#define dglBufferData(args...) \
-  dbg_gl_call(glBufferData, __FILE__, __LINE__, "glBufferData", args)
-#define dglDeleteBuffers(args...) \
-  dbg_gl_call(glDeleteBuffers, __FILE__, __LINE__, "glDeleteBuffers", args)
-#define dglDeleteVertexArrays(args...) \
-  dbg_gl_call(glDeleteVertexArrays, __FILE__, __LINE__, "glDeleteVertexArrays", args)
-#define dglDrawElements(args...) \
-  dbg_gl_call(glDrawElements, __FILE__, __LINE__, "glDrawElements", args)
-#define dglEnableVertexAttribArray(args...) \
-  dbg_gl_call(glEnableVertexAttribArray, __FILE__, __LINE__, "glEnableVertexAttribArray", args)
-#define dglGenBuffers(args...) \
-  dbg_gl_call(glGenBuffers, __FILE__, __LINE__, "glGenBuffers", args)
-#define dglGenVertexArrays(args...) \
-  dbg_gl_call(glGenVertexArrays, __FILE__, __LINE__, "glGenVertexArrays", args)
-#define dglVertexAttribPointer(args...) \
-  dbg_gl_call(glVertexAttribPointer, __FILE__, __LINE__, "glVertexAttribPointer", args)
+
 #endif //GL_CALLS_HXX
+EOM
+
+cat gfx.cxx game.cxx shader.cxx | awk "match($0, /\s+d(gl[^(]+)\([^)]*\)/, names){ print names[1] }"  | grep -v glfw | grep -v glew | sort | uniq |
+while read -r func_name ; do
+    echo "#define d(args...) \" >> "gl_calls.hxx"
+    echo "  dbg_gl_call(, __FILE__, __LINE__, \"\", args)" >> "gl_calls.hxx"
+done
+
+cat <<EOF > "gl_calls.cxx"
+#include "gl_calls.hxx"
+
+void dbg_gl_print_args_internal () {}
+void dbg_gl_print_args () {}
