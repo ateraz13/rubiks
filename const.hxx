ba74117eaedb07f8af5c610d56ef27c7ae8e0866
ateraz13 <mipsmartin@gmail.com>
2025-01-17 18:29:36 +0000

    Constant values.

---
+++ const.hxx
@@ -0,0 +1,8 @@
+#ifndef CONST_HXX
+#define CONST_HXX
+
+extern const int MAIN_WINDOW_DEFAULT_HEIGHT;
+extern const int MAIN_WINDOW_DEFAULT_WIDTH;
+extern const char *MAIN_WINDOW_DEFAULT_TITLE;
+
+#endif // CONST_HXX
