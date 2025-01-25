3f25cb0db5ba7d09d7b9716680d2c8a59918ede1
ateraz13 <mipsmartin@gmail.com>
2025-01-25 14:47:36 +0000

    Optional clean build!

--- build-wsl.sh
+++ build-wsl.sh
@@ -1,4 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-cmake -B./build-x86-64-mingw-win -DCMAKE_TOOLCHAIN_FILE=$PWD/mingw-w64-wsl.cmake -DUSE_GLAD=1
-cmake --build ./build-x86-64-mingw-win
+build_dir=./build-x86-64-mingw-win
+do_clean_build=0
+
+for x in "$@" ; do
+    if [ "$x" = "--clean-build" ] ; then
+        do_clean_build=1
+    fi
+done
+
+if [[ ! -d "$build_dir" ||  "$do_clean_build" -eq 1 ]]; then
+    rm -R "$build_dir"
+    cmake -B"$build_dir" -DCMAKE_TOOLCHAIN_FILE=$PWD/mingw-w64-wsl.cmake -DUSE_GLAD=1
+fi
+
+cmake --build "$build_dir"
