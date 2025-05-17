d82f20e304c7bcb60361a03c6ae182eb369072c6
ateraz13 <mipsmartin@gmail.com>
2025-05-17 18:48:35 +0100

    Adapt init.

--- main.cxx
+++ main.cxx
@@ -2,15 +2,16 @@
 #include "gfx.hxx"
 #include "game.hxx"
 #include "utility.hxx"
+#include "app.hxx"
 
 int main(int argc, char** argv) {
-    auto& game = Game::instance();
+    App &app = App::instance();
 
 #ifdef INSPECT_GL_DEBUG_INFO
     std::cout << "OpenGL call inspecting is enabled!\n";
 #endif
 
-    game.start();
+    app.init();
 
     std::cout << "Hello, world!\n";
 
