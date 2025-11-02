1b0a60bd3295e77dc082c6efd19b3044622a4a0b
ateraz13 <mipsmartin@gmail.com>
2025-11-02 15:05:30 +0000

    Unit testing incoming.

---
+++ tests/shader_parser_test.cxx
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <cmath>
+#include <file_includes/shader_parser_test_data.hxx>
+#include <rubiks/shader_parser.hxx>
+
+TEST(ShaderParserTest, some_fucking_test) {
+    EXPECT_EQ(std::max(10, 50), 50);
+
+    ShaderParser parser;
+
+
+
+}
